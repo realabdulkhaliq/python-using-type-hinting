3ebd7ec30b8c226c3d65b22b2e85dbfc0ab60579
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-12 15:45:14 +0500

    Dict vs dict

--- Basic/mix.py
+++ Basic/mix.py
@@ -5,4 +5,18 @@
 # names: list[Any] = [1, 2, "A"]
 
 # print([n**2 for n in range(1, 11)])
-grade: str| None = None
+# grade: str| None = None
+
+myDict: dict = {"name": "Abdul", "age": 30}
+
+print(myDict["name"])
+
+myDict2: dict[str, str] = {"name": "Abdul", "age": "30"}
+
+print(myDict2["name"])
+
+from typing import Dict
+
+myDict3: Dict[str, str] = {"name": "Abdul", "age": "30"}
+
+print(myDict3["name"])
